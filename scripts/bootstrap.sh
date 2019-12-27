63bb3ba36032d0e38b080ed5a5d3ff7a81951dff
HeavenOSK <ryunosukeheaven.ba@gmail.com>
2019-12-27 14:11:07 +0900

    add own license with securing original LICENSE

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -17,7 +17,7 @@ cp -r ${source}/lib ${name}
 cp -r ${source}/test ${name}
 cp -r ${source}/assets ${name}
 cp ../analysis_options.yaml ${name}
-cp ../LICENSE ${name}
+cp ../heavenosk_license/LICENSE ${name}
 sed -e "s/flutter_bootstrap/${name}/g" ${source}/pubspec.yaml > ${name}/pubspec.yaml
 
 open ${name}
